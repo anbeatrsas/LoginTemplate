01024b75f18cc2bb4601bdf17246d6c8bcfa1077
ANA BEATRIZ DE SOUZA ALMEIDA SILVEIRA <ana.bsasilveira@senacsp.edu.br>
2024-02-21 11:36:04 -0300

    JavaScript

--- js/comandos.js
+++ js/comandos.js
@@ -1,1 +1,11 @@
-console.log("adicionado");
+function validaNome(event){
+
+    var nome = $("#nome").val();
+    var regex = /[^(A-Za-z\u00C0-\u00FF )]+/i;
+
+    nome = nome.replace(regex, '');
+
+    $("#nome").val(nome);
+    $("#nome").focus();
+
+}
